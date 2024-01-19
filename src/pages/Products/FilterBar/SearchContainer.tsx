c6444ec6ecbc883c1bce86a65a4b97091fa2c263
Shehari931 <91692552+Shehari931@users.noreply.github.com>
2024-01-20 01:53:52 +0500

    updating favivon

--- src/pages/Products/FilterBar/SearchContainer.tsx
+++ src/pages/Products/FilterBar/SearchContainer.tsx
@@ -1,8 +1,9 @@
-import { Box, Container, useTheme } from "@mui/material";
+import { Box, Container, Typography, useTheme } from "@mui/material";
 import style from "./SearchContainer.module.css";
 import IconSecondaryBtn from "../../../utils/buttons/IconSecondaryBtn";
 import CloseIcon from "@mui/icons-material/Close";
 import SearchIcon from "@mui/icons-material/Search";
+import { Link } from "react-router-dom";
 
 interface Props {
   showSearch: boolean;
@@ -11,10 +12,11 @@ interface Props {
 
 const SearchContainer = ({ showSearch, showSearchHandler }: Props) => {
   const theme = useTheme();
+
   return (
     <Box
       sx={{
-        backgroundColor: theme.palette.background.default,
+        backgroundColor: theme.palette.secondary.main,
         position: "absolute",
         top: "0",
         left: "0",
@@ -22,43 +24,71 @@ const SearchContainer = ({ showSearch, showSearchHandler }: Props) => {
         height: "100vh",
         zIndex: "99",
         paddingTop: "2rem",
+        paddingInline: "2rem",
         display: showSearch ? "block" : "none",
       }}
     >
-      <Container>
+      {/* <Container> */}
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          gap: "1.5rem",
+        }}
+      >
         <Box
           sx={{
             display: "flex",
             justifyContent: "space-between",
             alignItems: "center",
-            gap: "1.5rem",
+            width: "100%",
+            gap: "1rem",
           }}
         >
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-              width: "100%",
-              gap: "1rem",
-            }}
-          >
-            <input
-              type="text"
-              placeholder="Search"
-              className={style.searchInput}
-            />
-            <IconSecondaryBtn
-              Type={SearchIcon}
-              clickEvent={() => console.log("hello from close")}
-            />
-          </Box>
+          <input
+            type="text"
+            placeholder="Search"
+            className={style.searchInput}
+          />
           <IconSecondaryBtn
-            Type={CloseIcon}
-            clickEvent={() => showSearchHandler()}
+            Type={SearchIcon}
+            clickEvent={() => console.log("hello from close")}
           />
         </Box>
-      </Container>
+        <IconSecondaryBtn
+          Type={CloseIcon}
+          clickEvent={() => showSearchHandler()}
+        />
+      </Box>
+      <Box sx={{ marginTop: "2rem" }}>
+        <Link to={"/"} style={{ textDecoration: "none" }}>
+          <Box sx={{ display: "flex", gap: "1rem", alignItems: "center" }}>
+            <Box sx={{ flexGrow: 1, maxWidth: "50px", aspectRatio: "1/1" }}>
+              <img
+                style={{ width: "100%" }}
+                src="https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg"
+                alt=""
+              />
+            </Box>
+            <Box sx={{ maxWidth: "300px" }}>
+              <Typography sx={{ color: theme.palette.background.default }}>
+                Jon Hardy Women's Legends Naga Gold & Silver Dragon Station
+                Chain Braceleth
+              </Typography>
+              <Typography
+                sx={{
+                  color: theme.palette.background.default,
+                  fontWeight: "600",
+                }}
+              >
+                22.5$
+              </Typography>
+            </Box>
+          </Box>
+        </Link>
+      </Box>
+      {/* </Container> */}
     </Box>
   );
 };
