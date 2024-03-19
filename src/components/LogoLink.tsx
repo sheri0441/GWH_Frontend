ea936af946367714b2488c4e5ef24dd691ab6c6f
Sheharyar <94967367+sheri0441@users.noreply.github.com>
2024-03-19 06:46:40 +0500

    Update LogoLink.tsx

--- src/components/LogoLink.tsx
+++ src/components/LogoLink.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/image/logo.png";
+import logo from "../assets/image/LOGO.png";
 import "./LogoLink.css";
 
 const LogoLink = () => {
